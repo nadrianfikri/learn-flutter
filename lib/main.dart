3ecc7475f3ee52b4f9ef439573f3558e8bc1d5d0
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2022-10-21 13:43:11 +0700

    feat: text widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,27 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Hello World App'),
         ),
-        body: const Center(child: Text('Hello world')),
+        body: Center(
+            child: Container(
+          color: Colors.cyan,
+          width: 150,
+          // height: 50,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Lorem ipsum dolor sit amet yang panjang sekali sampai tiga baris seharusnya',
+              // maxLines: 2,
+              // overflow: TextOverflow.ellipsis,
+              // softWrap: false,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20),
+            ),
+          ),
+        )),
       ),
     );
   }
