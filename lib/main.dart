31f0290f4fb86dd722edb6f44a9d97a4178509f8
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2022-10-21 14:21:35 +0700

    feat: container

--- lib/main.dart
+++ lib/main.dart
@@ -12,67 +12,84 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Hello World App'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              color: Colors.cyan,
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Column 1',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20),
+        body: Container(
+          color: Colors.blueGrey,
+          padding: const EdgeInsets.fromLTRB(10, 20, 30, 40),
+          margin: const EdgeInsets.all(20),
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: const LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[Colors.amber, Colors.blue])),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  color: Colors.cyan,
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Column 1',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Column 2',
+                Container(
+                  color: Colors.blue,
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Column 2',
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              color: Colors.lightGreen,
-              child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Column 3',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontStyle: FontStyle.italic,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 20),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                Container(
+                  color: Colors.lightGreen,
+                  child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
                         children: [
-                          Container(
-                              color: Colors.redAccent, child: Text('Row 1')),
-                          Container(
-                              color: Colors.pinkAccent, child: Text('Row 2')),
-                          Container(
-                              color: Colors.orangeAccent, child: Text('Row 3')),
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Column 3',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                  color: Colors.redAccent,
+                                  child: const Text('Row 1')),
+                              Container(
+                                  color: Colors.pinkAccent,
+                                  child: const Text('Row 2')),
+                              Container(
+                                  color: Colors.orangeAccent,
+                                  child: const Text('Row 3')),
+                            ],
+                          )
                         ],
-                      )
-                    ],
-                  )),
+                      )),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
